d33855b2c93621c4858ab1a05bdb635f89ca562d
OyeBenny <bennyv@protonmail.com>
2019-09-26 21:13:45 -0400

    Updated HTML and JS

--- script.js
+++ script.js
@@ -1,5 +1,39 @@
-window.addEventListener('keydown', function(e){
-    const audio = document.querySelector(`audio[data-key = "${e.keyCode}"]`)
-    console.log(audio);
-    if(!audio) return; // if audio key option is not available like the letter 'z' then it just stops.
-});
+function playSound(e) {
+    const audio = document.querySelector(`audio[data-key = "${e.keyCode}"]`);
+    const key = document.querySelector(`div[data-key = "${e.keyCode}"]`);
+    if (!audio) return; // if audio key option is not available, stops it
+    key.classList.add('playing');
+    audio.currentTime = 0; // rewind back to start so you can play sounds fast
+    audio.play();
+    
+}
+
+  function endSound (e) {
+    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+    if(!key) return; // if it doesn't, stops function
+    key.classList.remove('playing');
+  }
+
+window.addEventListener('keyup', endSound)
+window.addEventListener('keydown', playSound);
+
+// **Leaving this for historical reasons. Not sure why this script below only works inline inside <script> tags. The script above does work as JS from an src external file. The script below works if you want to include JS into your HTML document for some reason.**
+
+//function removeTransition(e) {
+//    if (e.propertyName !== 'transform') return; //skip if its not a transform
+//    e.target.classList.remove('playing');
+//}
+//
+//function playSound(e) {
+//    const audio = document.querySelector(`audio[data-key = "${e.keyCode}"]`);
+//    const key = document.querySelector(`div[data-key = "${e.keyCode}"]`);
+//    if (!audio) return; // if audio key option is not available, stops it
+//    key.classList.add('playing');
+//    audio.currentTime = 0; // rewind back to start so you can play sounds fast
+//    audio.play();
+//    
+//}
+//
+//const keys = document.querySelectorAll('.key');
+//keys.forEach(key => key.addEventListener('transitionend', removeTransition)); 
+//window.addEventListener('keydown', playSound);
